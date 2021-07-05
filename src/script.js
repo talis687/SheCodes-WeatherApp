eb6109ede3f836a824df04b08a173e925a9ee101
Talis687 <talis687@yahoo.com.au>
2021-07-05 11:37:12 +0800

    commit to master

--- src/script.js
+++ src/script.js
@@ -1,28 +1,28 @@
-let weather = {
-  "paris": {
+let weather = [
+  "paris"| {
     temp: 19.7,
     humidity: 80
   },
-  "tokyo": {
+  "tokyo"| {
     temp: 17.3,
     humidity: 50
   },
-  "lisbon": {
+  "lisbon"| {
     temp: 30.2,
     humidity: 20
   },
-  "san francisco": {
+  "san francisco"| {
     temp: 20.9,
     humidity: 100
   },
-  "moscow": {
+  "moscow"| {
     temp: -5,
     humidity: 20
   }
-};
+];
 
 let city = prompt("Which City Are You Looking For?")
 
-if (city.toLowerCase().trim() = "paris") 
-    {alert (` The temperture for ${ weather[0] } is ${ weather[0].temp } and its humidity is ${weather[0].humidity} `)
+if (city.toLowerCase().trim() = weather.forEach)
+    {alert (` The temperture for ${ weather.forEach } is ${ weather.forEach.temp } and its humidity is ${weather.forEach.humidity} `)
 }
