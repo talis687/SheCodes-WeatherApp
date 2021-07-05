0ed79212e71e518bad62290b04d5c2864d723876
Talis687 <talis687@yahoo.com.au>
2021-07-05 10:48:26 +0800

    commit to master

--- src/script.js
+++ src/script.js
@@ -25,6 +25,6 @@ let city = prompt("Which City Are You Looking For?")
 
 if (city = weather[0]) {
     alert = ` The temperture for ${ weather[0] } is ${ weather[0].temp } and its humidity is ${weather[0].humidity} `
-} else {
-    
+} else {if (city = weather[1]) 
+    alert = ` The temperture for ${ weather[1] } is ${ weather[1].temp } and its humidity is ${weather[1].humidity} 
 }
